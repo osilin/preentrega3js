2265a132c84d0c79c6f8d2e4b66023d254b29475
osilindo <abaltuliones@gmail.com>
2023-07-30 03:17:25 -0300

    Se sube html y estructura sin JS aun

---
+++ js/productos.js
@@ -0,0 +1,6 @@
+const productos = [{id: 1, nombre: "Vela de soja -modelo pecera-", precio: 2500, categoria: "Decoración"},
+                   {id: 2, nombre: "Vela de soja -modelo v. conico-", precio: 9800, categoria: "Decoración"},
+                   {id: 3, nombre: "Vela de soja -modelo vaso-", precio: 4600, categoria: "Decoración"},
+                   {id: 4, nombre: "Sahumo -romero-", precio: 670, categoria: "Limpieza-aromaterapia"},
+                   {id: 5, nombre: "Sahumerios -Sagrada Madre-", precio: 790, categoria: "Limpieza-aromaterapia"},
+                   {id: 6, nombre: "Piedras para defumación", precio: 670, categoria: "Limpieza-aromaterapia"}]
