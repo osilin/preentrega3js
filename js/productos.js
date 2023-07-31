4b3e8df90bea42a794f48df15d245e159e2c49c4
osilindo <abaltuliones@gmail.com>
2023-07-31 01:02:02 -0300

    Se arreglan rutas para imagenes

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
-const productos = [{ id: 1, nombre: "Vela de soja -modelo pecera-", imagen: "images/velaDeSojaPecera.jpg", precio: 2500, categoria: "Decoración" },
-{ id: 2, nombre: "Vela de soja -modelo v. conico-", imagen: "images/velaSojaFloreroVidrioConico.png", precio: 9800, categoria: "Decoración" },
-{ id: 3, nombre: "Vela de soja -modelo vaso-", imagen: "images/velaVaso.jpeg", precio: 4600, categoria: "Decoración" },
-{ id: 4, nombre: "Sahumo -romero-", imagen: "images/sahumoSmadre.jpg", precio: 670, categoria: "Limpieza-aromaterapia" },
-{ id: 5, nombre: "Sahumerios -Sagrada Madre-", imagen: "images/sahumerioSmadreElementos.jpg", precio: 790, categoria: "Limpieza-aromaterapia" },
-{ id: 6, nombre: "Piedras para defumación", imagen: "images/defumacionSmadre.png", precio: 670, categoria: "Limpieza-aromaterapia" }]
+const productos = [{ id: 1, nombre: "Vela de soja -modelo pecera-", imagen: "../images/velaDeSojaPecera.jpg", precio: 2500, categoria: "Decoración" },
+{ id: 2, nombre: "Vela de soja -modelo v. conico-", imagen: "../images/velaSojaFloreroVidrioConico.png", precio: 9800, categoria: "Decoración" },
+{ id: 3, nombre: "Vela de soja -modelo vaso-", imagen: "../images/velaVaso.jpeg", precio: 4600, categoria: "Decoración" },
+{ id: 4, nombre: "Sahumo -romero-", imagen: "../images/sahumoSmadre.jpg", precio: 670, categoria: "Limpieza-aromaterapia" },
+{ id: 5, nombre: "Sahumerios -Sagrada Madre-", imagen: "../images/sahumerioSmadreElementos.jpg", precio: 790, categoria: "Limpieza-aromaterapia" },
+{ id: 6, nombre: "Piedras para defumación", imagen: "../images/defumacionSmadre.png", precio: 670, categoria: "Limpieza-aromaterapia" }]
